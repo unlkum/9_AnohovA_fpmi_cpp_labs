8a1866932731d38e0f14d82d4a3b9c906829f2ac
Your Name <you@example.com>
2025-12-09 23:30:57 +0300

    Solve lab

--- semester_1/lab7_class_vector/vector/vector_impl.cpp
+++ semester_1/lab7_class_vector/vector/vector_impl.cpp
@@ -1,3 +1,117 @@
 #include "vector_impl.h"
 
-// your code goes here
+void Vector::Reallocate(size_t new_capacity) {
+    int* new_data = new int[new_capacity];
+
+    std::copy(data_, data_ + size_, new_data);
+
+    delete[] data_;
+    data_ = new_data;
+    capacity_ = new_capacity;
+}
+
+Vector::Vector() : size_(0), capacity_(0), data_(nullptr) {
+}
+
+Vector::Vector(size_t size)
+    : size_(size), capacity_(size), data_(size > 0 ? new int[size] : nullptr) {
+    for (size_t i = 0; i < size_; ++i) {
+        data_[i] = 0;
+    }
+}
+
+Vector::Vector(std::initializer_list<int> list)
+    : size_(list.size()),
+      capacity_(list.size()),
+      data_(list.size() > 0 ? new int[list.size()] : nullptr) {
+    std::copy(list.begin(), list.end(), data_);
+}
+
+Vector::Vector(const Vector& other)
+    : size_(other.size_), capacity_(other.capacity_), data_(new int[other.capacity_]) {
+    std::copy(other.data_, other.data_ + size_, data_);
+}
+
+Vector& Vector::operator=(Vector other) {
+    this->Swap(other);
+
+    return *this;
+}
+
+void Vector::Swap(Vector& other) {
+    std::swap(data_, other.data_);
+    std::swap(size_, other.size_);
+    std::swap(capacity_, other.capacity_);
+}
+
+int& Vector::At(size_t ind) {
+    if (ind >= size_) {
+        throw std::out_of_range("Vector::at() - Index out of range");
+    }
+    return data_[ind];
+}
+
+const int& Vector::At(size_t ind) const {
+    if (ind >= size_) {
+        throw std::out_of_range("Vector::at() - Index out of range");
+    }
+    return data_[ind];
+}
+
+size_t Vector::Size() const {
+    return size_;
+}
+
+size_t Vector::Capacity() const {
+    return capacity_;
+}
+
+void Vector::PushBack(const int item) {
+    if (size_ >= capacity_) {
+        size_t new_capacity = (capacity_ == 0) ? 1 : capacity_ * 2;
+        Reallocate(new_capacity);
+    }
+    data_[size_] = item;
+    ++size_;
+}
+
+void Vector::PopBack() {
+    if (size_ > 0) {
+        --size_;
+    }
+}
+
+void Vector::Clear() {
+    size_ = 0;
+}
+
+void Vector::Reserve(size_t new_capacity) {
+    if (capacity_ >= new_capacity) {
+        return;
+    }
+    Reallocate(new_capacity);
+}
+
+int& Vector::operator[](size_t ind) {
+    return data_[ind];
+}
+
+const int& Vector::operator[](size_t ind) const {
+    return data_[ind];
+}
+
+Vector::~Vector() {
+    delete[] data_;
+}
+
+std::ostream& operator<<(std::ostream& os, const Vector& v) {
+    os << "[";
+    for (size_t i = 0; i < v.Size(); ++i) {
+        os << v[i];
+        if (i + 1 < v.Size()) {
+            os << ", ";
+        }
+    }
+    os << "]";
+    return os;
+}
--- semester_1/lab7_class_vector/vector/vector_impl.h
+++ semester_1/lab7_class_vector/vector/vector_impl.h
@@ -1,5 +1,95 @@
 #pragma once
 
+#include <algorithm>
+#include <initializer_list>
+#include <cstddef>
+#include <stdexcept>
+#include <iostream>
+
 class Vector {
-    // your code goes here
+private:
+    size_t size_;
+    size_t capacity_;
+    int* data_;
+
+    void Reallocate(size_t new_capacity);
+
+public:
+    // Конструктор по умолчанию
+    Vector();
+
+    // Конструктор, принимающий размер вектора и заполняющий его нулями
+    explicit Vector(size_t size);
+
+    // Принимающий std::intitializer_list<int> — Vector v{1, 2, 3, 4, 5};
+    Vector(std::initializer_list<int> list);
+
+    // Конструктор копирования Vector b = a; или Vector b(a)
+    Vector(const Vector& other);
+
+    // Оператор присваивания копированием b = a;
+    Vector& operator=(Vector other);
+
+    // Метод Swap, который принимает другой вектор по ссылке и меняет содержимое текущего вектора
+    void Swap(Vector& other);
+
+    // Метод At (неконстантный)
+    int& At(size_t ind);
+
+    // Метод At (константный)
+    const int& At(size_t ind) const;
+
+    // Метод Size
+    size_t Size() const;
+
+    // Метод Capacity
+    size_t Capacity() const;
+
+    // Метод PushBack
+    void PushBack(const int item);
+
+    // Метод PopBack , который удаляет последний элемент вектора
+    void PopBack();
+
+    // Метод Clear , который делает контейнер пустым
+    void Clear();
+
+    // Метод Reserve принимающий новое capacity
+    void Reserve(size_t new_capacity);
+
+    // Оператор индексирования data[i] (для записи/чтения)
+    int& operator[](size_t ind);
+
+    // Константный оператор индексирования data[i] (только для чтения)
+    const int& operator[](size_t ind) const;  // для const Vector& v
+
+    // Деструктор
+    ~Vector();
+
+    /*
+    Vector(Vector&& other) noexcept
+        : size_(other.size_), capacity_(other.capacity_), data_(other.data_) {
+        other.size_ = 0;
+        other.capacity_ = 0;
+        other.data_ = nullptr;
+    }
+
+    Vector& operator=(Vector&& other) noexcept {
+        if (this != &other) {
+
+            delete[] data_;
+
+            size_ = other.size_;
+            capacity_ = other.capacity_;
+            data_ = other.data_;
+
+            other.size_ = 0;
+            other.capacity_ = 0;
+            other.data_ = nullptr;
+        }
+        return *this;
+    }
+    */
 };
+
+std::ostream& operator<<(std::ostream& os, const Vector& v);
